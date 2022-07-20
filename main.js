85ae5332e3a6791a8f8ebbe14c40dbe9d490d02f
Souriya PHONGSAVANH <benjamin.franklin59777@gmail.com>
2022-07-20 16:34:20 +0200

    remove Intense fonctionnality

--- main.js
+++ main.js
@@ -2,8 +2,3 @@ import "normalize.css"
 import './main.scss'
 import './slider.scss'
 import './slider.js'
-
-window.addEventListener('load', function () {
-  let elements = document.querySelectorAll('.zara-slider__images img');
-  Intense(elements);
-});
--- slider.js
+++ slider.js
@@ -1,6 +1,10 @@
 window.onload = function () {
   (function () {
-    console.debug("souriya-zara-slider.js 😎");
+    if (!document.querySelector(".zara-slider__images")) {
+      return;
+    }
+
+    console.log("souriya-zara-slider.js 😎");
 
     // UTILS
     let throttlePause;
@@ -25,6 +29,8 @@ window.onload = function () {
 
     // VARIABLES
     const IMAGES = document.querySelector(".zara-slider__images");
+    // FIXME INTENSE
+    // const MEDIAS = document.querySelectorAll('.zara-slider__images img');
     const THUMBNAIL_BUTTONS = document.querySelectorAll(
       ".zara-slider__thumbnails button"
     );
@@ -34,8 +40,19 @@ window.onload = function () {
       .childElementCount;
 
     let currentSlide = 1;
+    let pressed = false;
+    let hasMove = false;
+    let isUp;
+    let touchstartY = 0
+    let touchendY = 0
 
     // FUNCTIONS
+    function init() {
+      updateSlider();
+      // FIXME INTENSE
+      // Intense(MEDIAS);
+    }
+
     function getProgressStep() {
       return PROGRESS.clientHeight / TOTAL;
     }
@@ -68,77 +85,65 @@ window.onload = function () {
       updateSlider();
     }
 
-    // EVENTS
-    window.addEventListener('resize', () => {
-      debounce(updateSlider, 500);
-    });
-
+    // EVENTS  CLICK
     for (let i = 0; i < THUMBNAIL_BUTTONS.length; i++) {
       const button = THUMBNAIL_BUTTONS[i];
-
       button.addEventListener("click", () => {
-        // FIXME closure
         currentSlide = parseInt(button.dataset.slideNumber);
-        console.log('click thumbnails', currentSlide);
         updateSlider();
       });
     }
 
+    // EVENTS MOUSE
     IMAGES.addEventListener("wheel", (e) => {
       throttle(() => moveSlide(e), 1600);
       e.preventDefault();
     }, false);
 
-    // INIT
-    updateSlider();
 
-    const INNER_SLIDER = document.querySelector(".zara-slider__images ul");
-    let pressed = false;
-    let startY;
-    let y;
-    let initialY;
-    let isUp;
-    IMAGES.addEventListener('mousedown', (e) => {
-      console.log('mousedown');
-      pressed = true;
-      startY = e.offsetY - INNER_SLIDER.offsetTop;
-      IMAGES.style.cursor = "grabbing";
-    });
-    IMAGES.addEventListener("mouseenter", (e) => {
-      initialY = e.clientY;
-      // console.log('mouseenter', initialY);
+    IMAGES.addEventListener("mouseenter", () => {
       IMAGES.style.cursor = "grab";
     });
+
+    IMAGES.addEventListener('mousedown', () => {
+      pressed = true;
+      IMAGES.style.cursor = "grabbing";
+    });
+
     IMAGES.addEventListener("mouseup", () => {
-      console.log('mouseup', 'isUp', isUp);
       IMAGES.style.cursor = "grab";
       pressed = false;
 
-      currentSlide = isUp ? currentSlide + 1 : currentSlide - 1;
-      updateSlider();
+      if ((currentSlide === 1 && !isUp) || (currentSlide === TOTAL && isUp)) {
+        return;
+      }
+
+      if (hasMove) {
+        // FIXME INTENSE
+        // console.log('mouseup intense', '🛑', currentSlide, isUp);
+        // MEDIAS.forEach(elt => {
+        //   elt.addEventListener('click', stopIntense, true);
+        // });
+
+        currentSlide = isUp ? currentSlide + 1 : currentSlide - 1;
+        updateSlider();
+      } else {
+        // FIXME INTENSE
+        // console.log('mouseup intense', '✅');
+        // MEDIAS.forEach(elt => {
+        //   elt.removeEventListener('click', stopIntense);
+        // });
+      }
     });
+
     IMAGES.addEventListener("mousemove", (e) => {
       if (!pressed) return;
       e.preventDefault();
-      y = e.offsetY;
-      if (e.movementY > 0 && e.movementX == 0) {
-        isUp = false;
-      } else if (e.movementY < 0 && e.movementX == 0) {
-        isUp = true;
-        y *= -1;
-      }
-      console.log('mousemove', isUp, initialY, y, y - initialY);
-      //IMAGES.style.transform = `translateY(${y-initialY}px)`;
+      hasMove = true;
+      isUp = (e.movementY < 0 && e.movementX == 0) ? true : false;
     });
 
-
-    //////
-    // TODO
-    // click GIANT
-
-    let touchstartY = 0
-    let touchendY = 0
-
+    // MOBILE TOUCH
     function isTouchUp() {
       return (touchendY < touchstartY) ? true : false;
     }
@@ -153,5 +158,14 @@ window.onload = function () {
       currentSlide = isTouchUp() ? currentSlide + 1 : currentSlide - 1;
       updateSlider();
     })
+
+    // EVENTS RESIZE
+    window.addEventListener('resize', () => {
+      debounce(updateSlider, 500);
+    });
+
+    // INIT
+    init();
+
   })();
 };
